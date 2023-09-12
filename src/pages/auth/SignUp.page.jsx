0fbc6454aa8124c56b796e5fd4bb5ad9fe4da72f
logasubramanism <smlogasubramani@gmail.com>
2023-09-12 23:43:40 +0530

    register page done

--- src/pages/auth/SignUp.page.jsx
+++ src/pages/auth/SignUp.page.jsx
@@ -1,9 +1,96 @@
-const SignUPPage = () => {
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for redirection (if using React Router)
+import '../../css/signup.css'; // Import your CSS file for styling
+
+const SignUpPage = () => {
+    // Define state variables to store user input
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [aadharNumber, setAadharNumber] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Function to handle form submission
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        // You can add code here to process the user's registration data
+    };
+
     return (
-        <div>
-            <h1>This is SignUPPage.</h1>
+        <div className="signup-container">
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSignUp}>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="Aadhar Number"
+                        value={aadharNumber}
+                        onChange={(e) => setAadharNumber(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="tel"
+                        placeholder="Phone Number"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+            <p>Already have an account? <Link to="/signin">Sign In</Link></p>
         </div>
     );
 }
 
-export default SignUPPage;
+export default SignUpPage;
+
