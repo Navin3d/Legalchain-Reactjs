61c6300feb1baa70e707d8d3e84bbd0a643bf9f7
logasubramanism <smlogasubramani@gmail.com>
2023-09-20 09:06:27 +0530

    share button mui

--- src/pages/document/DocList.page.jsx
+++ src/pages/document/DocList.page.jsx
@@ -1,161 +1,142 @@
-    // import { useEffect, useState } from "react";
-    // import axios from "axios";
-    // import '../../css/upload.css';
-
-    // const DocListPage = () => {
-    //     const INITIAL = [
-        
-    //         { timeStamp: '2023-09-12', tittle: 'Section 372 criminal case' },
-    //         { timeStamp: '2023-09-13', tittle: 'Section 432 Overspeeding case' },
-    //         { timeStamp: '2023-09-12', tittle: 'Section 372 criminal case' },
-    //         { timeStamp: '2023-09-13', tittle: 'Section 432 Overspeeding case' },
-    
-    //     ];
-
-    //     const [documents, setDocuments] = useState(INITIAL);
-
-    //     useEffect(() => {
-    //         getData();
-    //     }, []);
-
-    //     const getData = async () => {
-    //         const { data } = await axios.get("http://localhost:8080/user/docs/logamis");
-    //         setDocuments((prev) => []);
-    //         console.log(data);
-    //         if(data["ownedRecords"].length > 0) {
-    //             setDocuments((prev) => (data["ownedRecords"]));
-    //         }
-    //         if(data["sharedRecords"].length > 0) {
-    //             setDocuments((prev) => ([ ...prev, ...data["sharedRecords"] ]));
-    //         }
-    //     }
-
-    //     return (
-    //         <div className="background">
-    //             <div className="container">
-    //                 <div className="row">
-    //                     <div className="col-12 text-center">
-    //                         <h2 className='upload'>Case Files</h2>
-    //                     </div>
-    //                 </div>
-    //                 <div className="row">
-    //                     {documents.map((document, index) => (
-    //                         <div key={index} className="col-md-6 mb-4" id='card-start'>
-    //                             <div className="card">
-    //                                 <div className="card-body">
-    //                                     <h5 className="card-title">Case: {document.tittle}</h5>
-    //                                     <p className="card-text">Date: {document.timeStamp}</p>
-    //                                     <div className="row">
-    //                                     <div className="col-md-5">
-    //                                     <a href={document.hash} target="_blank" className="btn btn-primary" id='button-css'>View</a>
-    //                                     </div>
-    //                                     <div className="col-md-5">
-    //                                     <a href={document.hash} target="_blank" className="btn btn-primary" id='button-css'>share</a>
-    //                                     </div>
-    //                                     </div>                               
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     ))}
-    //                 </div>  
-    //             </div>
-    //         </div>
-    //     );
-    // }
-
-    // export default DocListPage;
-
-
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import '../../css/upload.css';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { blue } from '@mui/material/colors';
+import PersonIcon from '@mui/icons-material/Person';
+import PropTypes from 'prop-types'; // Add this import
 
-const DocListPage = () => {
-    const INITIAL = [
-        { timeStamp: '2023-09-12', tittle: 'Section 372 criminal case' },
-{ timeStamp: '2023-09-13', tittle: 'Section 432 Overspeeding case' },
-{ timeStamp: '2023-09-12', tittle: 'Section 372 criminal case' },
-{ timeStamp: '2023-09-13', tittle: 'Section 432 Overspeeding case' },
-    ];
+const emails = ['username@gmail.com', 'user02@gmail.com'];
 
-    const [documents, setDocuments] = useState(INITIAL);
-    const [openDialog, setOpenDialog] = useState(false); // State to control the dialog
+function SimpleDialog(props) {
+  const { onClose, selectedValue, open } = props;
+  
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
 
-    useEffect(() => {
-        getData();
-    }, []);
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
 
-    const getData = async () => {
-                    const { data } = await axios.get("http://localhost:8080/user/docs/logamis");
-setDocuments((prev) => []);
-console.log(data);
-if(data["ownedRecords"].length > 0) {
-    setDocuments((prev) => (data["ownedRecords"]));
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Share with the people</DialogTitle>
+      <List sx={{ pt: 0 }}>
+        {emails.map((email) => (
+          <ListItem disableGutters key={email}>
+            <ListItemButton onClick={() => handleListItemClick(email)}>
+              <ListItemAvatar>
+                <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                  <PersonIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={email} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        <ListItem disableGutters>
+          <ListItemButton
+            autoFocus
+            onClick={() => handleListItemClick('addAccount')}
+          >
+            <ListItemText primary="SHARE" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Dialog>
+  );
 }
-if(data["sharedRecords"].length > 0) {
-    setDocuments((prev) => ([ ...prev, ...data["sharedRecords"] ]));
-}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
+
+export default function DocListPage() {
+  const INITIAL = [
+    { timeStamp: '2023-09-12', tittle: 'Section 372 criminal case' },
+    { timeStamp: '2023-09-13', tittle: 'Section 432 Overspeeding case' },
+    { timeStamp: '2023-09-12', tittle: 'Section 372 criminal case' },
+    { timeStamp: '2023-09-13', tittle: 'Section 432 Overspeeding case' },
+  ];
+
+  const [documents, setDocuments] = useState(INITIAL);
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:8080/user/docs/logamis");
+      setDocuments((prev) => []);
+      console.log(data);
+      if (data["ownedRecords"].length > 0) {
+        setDocuments((prev) => data["ownedRecords"]);
+      }
+      if (data["sharedRecords"].length > 0) {
+        setDocuments((prev) => [...prev, ...data["sharedRecords"]]);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  };
 
-    const handleShareButtonClick = () => {
-        // Open the dialog
-        setOpenDialog(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleCloseDialog = () => {
-        // Close the dialog
-        setOpenDialog(false);
-    };
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
 
-    return (
-        <div className="background">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 text-center">
-                        <h2 className='upload'>Case Files</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    {documents.map((document, index) => (
-                        <div key={index} className="col-md-6 mb-4" id='card-start'>
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Case: {document.tittle}</h5>
-                                    <p className="card-text">Date: {document.timeStamp}</p>
-                                    <div className="row">
-                                        <div className="col-md-5">
-                                            <a href={document.hash} target="_blank" className="btn btn-primary" id='button-css'>View</a>
-                                        </div>
-                                        <div className="col-md-5">
-                                        <a href={document.hash} target="_blank" className="btn btn-primary" id='button-css' onClick={handleShareButtonClick}>share</a>
-                                         {/* <button className="btn btn-primary" >Share</button> */}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <Dialog  open={openDialog} onClose={handleCloseDialog} >
-                <DialogTitle className="dialogue-box-title" >Share with:</DialogTitle>
-                <DialogContent>
-                    <p>Kaushik</p>
-                    <p>Navin Durai sm</p>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseDialog} color="primary">Close</Button>
-                </DialogActions>
-            </Dialog>
+  return (
+    <div className="background">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h2 className='upload'>Case Files</h2>
+          </div>
         </div>
-    );
+        <div className="row">
+          {documents.map((document, index) => (
+            <div key={index} className="col-md-6 mb-4" id='card-start'>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Case: {document.tittle}</h5>
+                  <p className="card-text">Date: {document.timeStamp}</p>
+                  <div className="row">
+                    <div className="col-md-5">
+                      <a href={document.hash} target="_blank" className="btn btn-primary" id='button-css'>View</a>
+                    </div>
+                    <div className="col-md-5">
+                      <a href={document.hash} target="_blank" className="btn btn-primary" id='button-css' onClick={handleClickOpen} >Share</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+          <SimpleDialog
+            selectedValue={selectedValue}
+            open={open}
+            onClose={handleClose}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default DocListPage;
-
